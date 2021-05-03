f4db274bad3b7cfbed91e3943f043678c3ee14a8
Eric <ericvsolcruz@gmail.com>
2021-05-03 08:39:39 -0400

    fixed

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  TOKEN: 'ODExNDI0NjIxOTkwNzcyNzU2.YCyAEQ.25yicBxCEAEMoEc-2qsgMdFJi1U',
-};
-
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const allIntents = new Intents(Intents.ALL);
-const client = new Client({ws: { intents: allIntents }});
-const config = require('./config');
+const client = new Client({ ws: { intents: allIntents } });
 
 const renewalID = '474064320167280642';
 const generalRoleID = '748369916075442336';
@@ -11,33 +11,53 @@ const generalChannelID = '757351401725624441';
 // 474064320167280642 Renewal Role ID
 
 async function checkRoles(oldRoles, newRoles) {
-  // No change in roles, return
-  if (oldRoles.length === newRoles.length) return false;
-  const oldRole = oldRoles.find(role => role === renewalID);
+    // No change in roles, return
+    if (oldRoles.length === newRoles.length) return false;
+    const oldRole = oldRoles.find((role) => role === renewalID);
 
-  // Already had Renewal role, return
-  if (oldRole) return false;
-  const foundRole = newRoles.find(role => role === renewalID);
+    // Already had Renewal role, return
+    if (oldRole) return false;
+    const foundRole = newRoles.find((role) => role === renewalID);
 
-  // If Renewal role not added, return
-  if (!foundRole) return false;
-  return true;
+    // If Renewal role not added, return
+    if (!foundRole) return false;
+    return true;
+}
+
+async function sleep() {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 200);
+    });
+}
+
+async function sendPing(userId) {
+    const pingChannel = client.channels.cache.get(generalChannelID);
+    pingChannel.send(
+        `Welcome <@${userId}> :smile: Please be sure to check out #how-to-set-up-discord and #server-map for information on everything you need to know to get started in the server. If you have any other questions, be sure to let us know!`
+    );
 }
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
-  const newUserId = newMember.id;
-  const oldRoles = oldMember.roles.cache.array().map((role) => role.id);
-  const newRoles = newMember.roles.cache.array().map((role) => role.id);
+    const newUserId = newMember.id;
+    const oldRoles = oldMember.roles.cache.array().map((role) => role.id);
+    const newRoles = newMember.roles.cache.array().map((role) => role.id);
 
-  const addedRenewal = await checkRoles(oldRoles, newRoles);
-  if (!addedRenewal) return;
+    const addedRenewal = await checkRoles(oldRoles, newRoles);
+    if (!addedRenewal) return;
 
-  const pingChannel = client.channels.cache.get(generalChannelID);
-  pingChannel.send(`Welcome <@${newUserId}> :smile: Please be sure to check out #how-to-set-up-discord for information on everything you need to know to get started in the server. If you have any other questions, be sure to let us know!`);
+    await sendPing(newUserId);
+});
+
+client.on('guildMemberAdd', async (member) => {
+    await sleep();
+    const role = member.roles.cache.find((role) => role.id == renewalID);
+    if (!role) return;
+
+    await sendPing(member.id);
 });
 
 client.on('ready', () => {
-  console.log('Logged in.');
+    console.log('Logged in Renewal Bot.');
 });
 
-client.login(config.TOKEN);
+client.login(process.env.DISCORD_TOKEN || '');
